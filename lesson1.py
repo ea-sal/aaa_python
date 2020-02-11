c0ebb7b8cc64a8f073126bcad02a2e7e924cb0e5
Ekaterina Saltykova <easaltykova@avito.ru>
2020-02-11 19:15:11 +0300

    adding step2

--- lesson1.py
+++ lesson1.py
@@ -5,6 +5,7 @@ def step1():
         'Торговый центр такой большой и страшный... '
         'Куда пойти в первую очередь? \n'
     )
+    global options
     options = {'1': '1. Папа был голодный, может, они в кафе?',
                '2': '2. Наверное остались в магазине игрушек, пойду туда.',
                '3': '3. Мама выглядела уставшей... Надо проверить бар.'}
@@ -47,7 +48,7 @@ def goto_cafe():
         elif answer == '2':
             print('В японском ресторанчике тетенька на входе сказала, '
                   'что мест не было и мама и папа пошли в бар напротив... '
-                  'Надо посмотреть там')
+                  'Надо посмотреть там <идет в бар>\n')
             return goto_bar()
         elif answer == '3':
             print('Кальян воняет отвратительно, и у мамы на него аллергия... Надо проверить что-то еще')
@@ -55,17 +56,55 @@ def goto_cafe():
 
 
 def goto_shop():
-    print('zhopa2')
-    return
+    print('Игрушки! Наверное они ищут мне подарок. \n'
+          'В каком отделе поискать?\n')
+    options_shop = {'1': '1. Куклы! Яжидевочка',
+                    '2': '2. Может, конструкторы? Обожаю лего',
+                    '3': '3. Машинки! Мама против гендерных стереотипов'}
+
+    for option, value in options_shop.items():
+        print(value)
+
+    next_step = ''
+    while not next_step:
+        if options_shop:
+            print('(пожалуйста, введите номер ответа):\n')
+        answer = input()
+        next_step = options_shop.get(answer, False)
+        if next_step and options_shop:
+            print('Ой, родителей тут нет... Поищу в другом отделе')
+            options_shop.pop(answer)
+            for option, value in options_shop.items():
+                print(value)
+                next_step = ''
+        if not options_shop:
+            print('\nОтделы закончились :( Нужно поискать в другом месте'
+                  'Куда пойти дальше?')
+            options.pop('2')
+            for option, value in options.items():
+                print(value)
+            next_step = ''
+            while not next_step:
+                print('(пожалуйста, введите номер ответа):\n')
+                answer = input()
+                next_step = options.get(answer, False)
+            if answer == '1':
+                return goto_cafe()
+            else:
+                return goto_bar()
+
+
+            return
 
 
 def goto_bar():
-    print('zhopa3')
+    print('Ура-урв, а вот и они! Я нашла родителей!'
+          '\nThe end :)')
     return
 
 
-#if name == '__main__':
-step1()
+if __name__ == '__main__':
+    step1()
 
 
 
