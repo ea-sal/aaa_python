2f8cea9cc576aa04a758aed6b488c99468d96eec
Ekaterina Saltykova <easaltykova@avito.ru>
2020-03-24 22:27:54 +0300

    hw3

--- lesson3_hw.py
+++ lesson3_hw.py
@@ -1,3 +1,31 @@
+'abcde'
+
+
+field_size = input()
+real_letters = 'abcde'[:field_size]
+for index, letter in enumerate(real_letters, start=1):
+    print(f'')
+
+
+PLAYER_SYMBOL = {
+    0: ' ',
+    1: 'X',
+    -1: 'O',
+}
+
+
+
+def display_row(data: list, label: str = ' '):
+    return ' | '.join(data)
+
+
+
+def convert_real_data_to_display_data(real_data: list) -> list:
+    # [-1, 0, 0]
+    # ['X', ' ', ' ']
+
+
+
 def choose_field_size():
     """a function that crafts a field"""
     while True:
@@ -47,31 +75,50 @@ e   |   |   |   |""")
     return field, rows, columns
 
 
-def player_x(field: list, rows: dict, columns: tuple) -> list:
+def player_x(rows: dict, columns: list):
     """a function that determines player X"""
     print('Игрок 1, ваш ход')
     while True:
         print('Пожалуйста, введите номер строки и столбца (например, а3)')
         move = list(input())
-        print(move)
         try:
-            chosen_row_num = rows.get(move[0])
+            chosen_row_num_x = rows.get(move[0])
         except KeyError:
             print('Первым символом должна быть маленькая латинская буква')
             continue
         if int(move[1]) in columns:
-            chosen_column_num = int(move[1])
+            chosen_column_num_x = int(move[1])
             break
         else:
             print('Вторым символом должна быть цифра от 1 до 3')
-    return print(chosen_row_num, chosen_column_num)
+    return chosen_row_num_x-1, chosen_column_num_x-1
 
 
-def player_o() -> int:
+def player_o(rows: dict, columns: list):
     """a function that determines player O"""
-    return
+    print('Игрок 2, ваш ход')
+    while True:
+        print('Пожалуйста, введите номер строки и столбца (например, а3)')
+        move = list(input())
+        try:
+            chosen_row_num_o = rows.get(move[0])
+        except KeyError:
+            print('Первым символом должна быть маленькая латинская буква')
+            continue
+        if int(move[1]) in columns:
+            chosen_column_num_o = int(move[1])
+            break
+        else:
+            print('Вторым символом должна быть цифра от 1 до 3')
+    return chosen_row_num_o-1, chosen_column_num_o-1
 
 
 if __name__ == '__main__':
     det_field, det_rows, det_columns = choose_field_size()
-    player_x(det_field, det_rows, det_columns)
+    while True:
+        move_x_row, move_x_column = player_x(det_rows, det_columns)
+        det_field[move_x_row][move_x_column] = [' x ']
+        move_o_row, move_o_column = player_o(det_rows, det_columns)
+        det_field[move_o_row][move_o_column] = [' o ']
+        print(det_field)
+        break
